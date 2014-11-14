c264ec79f02192d758321d63a6e64ef14c5c926d
Boaz Rosenan <brosenan@gmail.com>
2014-11-14 20:13:23 +0200

    >     atom should allow setting and getting a value >   ․ atom should allow setting and getting a value >     counter should add the given amount to the counter and return the value >   ․ counter should add the given amount to the counter and return the value 137c141 <   67 passing (87ms) --- >   69 passing (93ms)

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-mocha -C -R list --harmony | sed 's/:[^:]*$//' > .new.tr
+find . -maxdepth 2 -name test -type d | xargs mocha -C -R list --harmony | sed 's/:[^:]*$//' > .new.tr
 comment=`diff .old.tr .new.tr | sed 1d || echo`
 mocha -R markdown --harmony > spec.md
 mv .new.tr .old.tr
