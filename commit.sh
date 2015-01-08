d2ab03adc045cdef40b9a851ebbe0a2a143bdd65
Boaz Rosenan <brosenan@gmail.com>
2015-01-08 19:52:27 +0000

    configuring which directories are tested at all times

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-find . -maxdepth 2 -name test -type d | xargs ./node_modules/mocha/bin/mocha -C -R list --harmony | sed 's/:[^:]*$//' > .new.tr
+grep -v '^#' ./dirs-to-test | xargs ./node_modules/mocha/bin/mocha -C -R list --harmony | sed 's/:[^:]*$//' > .new.tr
 comment=`diff .old.tr .new.tr | sed 1d || echo`
 ./node_modules/mocha/bin/mocha -R markdown --harmony > spec.md
 mv .new.tr .old.tr
