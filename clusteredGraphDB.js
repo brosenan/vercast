9e6ae5118fdf0a36f8ee9bf15104fe266f5d5506
Boaz Rosenan <brosenan@gmail.com>
2015-01-14 14:08:32 +0200

    <   233 passing (466ms) --- >   233 passing (504ms)

--- clusteredGraphDB.js
+++ clusteredGraphDB.js
@@ -2,7 +2,7 @@
 var vercast = require('vercast');
 
 module.exports = function(externalGraphDB, bucketStore, capacity, clusteringFunc) {
-    var cache = new vercast.GraphCache(capacity);
+    var cache = new vercast.GraphCache(capacity, true);
 
     this.clear = function*() {
 	yield* externalGraphDB.clear();
@@ -14,45 +14,54 @@ module.exports = function(externalGraphDB, bucketStore, capacity, clusteringFunc
 	var c2 = clusteringFunc(v2);
 	if(c1 !== c2) {
 	    yield* externalGraphDB.addEdge(c1, e, c2);
+	    yield* bucketStore.append(c2, [{v1: v1, e: e, v2: v2}]);
 	}
 	yield* bucketStore.append(c1, [{v1: v1, e: e, v2: v2}]);
 	yield* cache.addEdge(v1, e, v2);
+	yield* cache.cleanup();
     };
     this.queryEdge = function*(v1, e) {
-//	if(!cache.has(v1)) {
-//	    yield* extractBucket(clusteringFunc(v1));
-//	}
-	return yield* cache.queryEdge(v1, e);
+	yield* extractBucket(clusteringFunc(v1));
+	var res = yield* cache.queryEdge(v1, e);
+	yield* cache.cleanup();
+	return res;
     };
     this.queryBackEdge = function*(v2, e) {
-	return yield* cache.queryBackEdge(v2, e);
+	yield* extractBucket(clusteringFunc(v2));
+	var res = yield* cache.queryBackEdge(v2, e);
+	yield* cache.cleanup();
+	return res;
     };
     this.findCommonAncestor = function*(v1, v2) {
 	var c1 = clusteringFunc(v1);
 	var c2 = clusteringFunc(v2);
-// 	if(c1 !== c2) {
-//	    var clusterLCA = yield* externalGraphDB.findCommonAncestor(c1, c2);
-//	    yield* extractBucket(clusterLCA.node);
-//	    for(let i = 0; i < clusterLCA.p1.length; i++) {
-//		yield* extractBucket(clusterLCA.p1[i].n);
-//	    }
-//	    for(let i = 0; i < clusterLCA.p2.length; i++) {
-//		yield* extractBucket(clusterLCA.p2[i].n);
-//	    }
-//	}
-	return yield* cache.findCommonAncestor(v1, v2);
+ 	if(c1 !== c2) {
+	    var clusterLCA = yield* externalGraphDB.findCommonAncestor(c1, c2);
+	    yield* extractBucket(clusterLCA.node);
+	    for(let i = 0; i < clusterLCA.p1.length; i++) {
+		yield* extractBucket(clusterLCA.p1[i].n);
+	    }
+	    for(let i = 0; i < clusterLCA.p2.length; i++) {
+		yield* extractBucket(clusterLCA.p2[i].n);
+	    }
+	}
+	var res = yield* cache.findCommonAncestor(v1, v2);
+	yield* cache.cleanup();
+	return res;
     };
     this.findPath = function*(v1, v2) {
 	var c1 = clusteringFunc(v1);
 	var c2 = clusteringFunc(v2);
-// 	if(c1 !== c2) {
-//	    var clusterLCA = yield* externalGraphDB.findCommonAncestor(c1, c2);
-//	    yield* extractBucket(clusterLCA.node);
-//	    for(let i = 0; i < clusterLCA.p2.length; i++) {
-//		yield* extractBucket(clusterLCA.p2[i].n);
-//	    }
-//	}
-	return yield* cache.findPath(v1, v2);
+ 	if(c1 !== c2) {
+	    var clusterLCA = yield* externalGraphDB.findCommonAncestor(c1, c2);
+	    yield* extractBucket(clusterLCA.node);
+	    for(let i = 0; i < clusterLCA.p2.length; i++) {
+		yield* extractBucket(clusterLCA.p2[i].n);
+	    }
+	}
+	var res = yield* cache.findPath(v1, v2);
+	yield* cache.cleanup();
+	return res;
     };
 
     function* extractBucket(id) {
--- test/clusteredGraphDB-test.js
+++ test/clusteredGraphDB-test.js
@@ -13,7 +13,7 @@ var bucketStore = new vercast.DummyBucketStore();
 var identity = function(id) { return id; };
 var universal = function(id) { return 'oneAndOnly'; };
 
-var graphDB1 = new vercast.ClusteredGraphDB(new vercast.DummyGraphDB(), bucketStore, 100, identity);
+var graphDB1 = new vercast.ClusteredGraphDB(new vercast.DummyGraphDB(), bucketStore, 1, identity);
 var graphDB2 = new vercast.ClusteredGraphDB(new vercast.GraphCache(1), bucketStore, 100, universal);
 
 describe('ClusteredGraphDB', function(){
@@ -23,12 +23,4 @@ describe('ClusteredGraphDB', function(){
     describe('with universal clustering', function(){
 	require('./graphDB-test.js')(graphDB2);
     });
-//    it('should be able to find paths in graphs bigger than the cache, as long as the path is smaller', function(done){
-//	var graphDB = new vercast.ClusteredGraphDB(new vercast.DummyGraphDB(),
-//						   new vercast.DummyBucketStore(),
-//						   10,
-//						   identity);
-//	
-//    });
-
 });
